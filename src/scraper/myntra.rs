c0a5529df3277bbd64351623cc69d74af89305e6
Sidharth-Singh10 <sidharthsingh714@gmail.com>
2025-01-26 14:38:58 +0530

    headless_mode

--- src/scraper/myntra.rs
+++ src/scraper/myntra.rs
@@ -12,7 +12,7 @@ pub async fn scrape_product(url: &str) -> Result<f32, Box<dyn std::error::Error>
              OsStr::new("--window-size=1920,1080"),
              OsStr::new("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
         ])
-        .headless(false)
+        .headless(true)
         .build()?;
 
     let browser = Browser::new(options)?;
